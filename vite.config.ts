058545d0ad66d62e7e47a0deaad9e22ed4c73405
qzda <qinzhidaoa@gmail.com>
2026-03-09 10:54:01 +0800

    chore: update manualChunks config

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
       output: {
         manualChunks: {
           vue: ["vue"],
-          jszip: ["jszip"],
+          tools: ["jszip", "chardet"],
         },
       },
     },
